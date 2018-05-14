eba3d13a143445ea038da679da0870987896d7d7
Rolando Conejo <rolandoconejo.cr@gmail.com>
2018-05-13 19:01:22 -0600

    fist commit in this project

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class HelloWorld {	
+	static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+  	static PrintStream out = System.out;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		out.println("Hello World!");
+
+	}
+
+}
